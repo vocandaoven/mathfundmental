c5b896e284aaa5f1cc26d1f23009e8265b6bea31
PFpifu <2350873852@qq.com>
2023-12-26 19:08:34 +0800

    generate the coe file

--- test/rom_generate.py
+++ test/rom_generate.py
@@ -1,9 +1,10 @@
+
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-img = Image.open("input.png")
+img = Image.open("input.bmp")
 pix = img.load()
 width = img.size[0]
 height = img.size[1]
